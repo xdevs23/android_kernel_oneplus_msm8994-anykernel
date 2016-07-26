1a5ac0a5df64e3ab9fdec9be3ee108f26bb9d943
xdevs23 <xdevs23@outlook.com>
2016-07-26 17:05:03 +0200

    Use android platform build system for anykernel

---
+++ Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+LOCAL_DIR := $(LOCAL_PATH)
+
+include $(CLEAR_VARS)
+
+LOCAL_PATH := $(LOCAL_DIR)
+
+LOCAL_MODULE := anykernel
+
+LOCAL_KERNEL_VERSION := blueberry-kernel-v1.0-bh3.6
+LOCAL_KERNEL_AUTHOR := xdevs23
+LOCAL_KERNEL_AUTHOR_ENDSTRING :=
+
+ifneq ($(LOCAL_KERNEL_AUTHOR),)
+    LOCAL_KERNEL_AUTHOR_ENDSTRING = -$(LOCAL_KERNEL_AUTHOR)
+endif
+
+ANYKERNEL_SH := $(LOCAL_DIR)/anykernel.zip
+
+$(ANYKERNEL_SH):
+	@if [ ! -e "$(OUT)/kernel" ]; then echo -e "\033[91mNo kernel image found! Please build the kernel using 'make bootimage' first.\033[0m"; exit 1; fi
+	@echo "Building AnyKernel package..."
+	cp $(OUT)/kernel $(LOCAL_DIR)/Image
+	zip -r $(OUT)/anykernel.zip $(LOCAL_DIR)/* -x README
+	mv $(OUT)/anykernel.zip $(OUT)/$(LOCAL_KERNEL_VERSION)$(LOCAL_KERNEL_AUTHOR_ENDSTRING).zip
+	@echo "AnyKernel package is made."
+
+all_modules: $(ANYKERNEL_SH)
