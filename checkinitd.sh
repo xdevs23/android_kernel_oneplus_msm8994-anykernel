99d1bc4823847346cb721bc1059c1fa16af1fea7
Simao Gomes Viana <xdevs23@outlook.com>
2016-08-19 10:40:28 +0200

    update zip: add a great init.d check

---
+++ checkinitd.sh
@@ -0,0 +1,9 @@
+#!/sbin/sh
+
+if [ ! -f /system/bin/sysinit ] || [[ "$(cat /system/bin/sysinit)" != *"init.d"* ]]; then
+    if [ -f /system/xbin/busybox ]; then
+        echo -en "\n# init.d\n/system/xbin/busybox run-parts /system/etc/init.d\n">>/system/bin/sysinit
+    else
+        echo -en "\n# init.d\nALLINITS="'"$(ls /system/etc/init.d/)"'"\nfor "'$initd in $ALLINITS; do'"\n"'/system/bin/sh /system/etc/init.d/$initd'"\ndone\n">>/system/bin/sysinit
+    fi
+fi
