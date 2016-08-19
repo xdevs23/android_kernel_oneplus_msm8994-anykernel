f6304246e13e81eb25e86f1958b15d08dc34edf5
Simao Gomes Viana <xdevs23@outlook.com>
2016-08-19 19:21:31 +0200

    checkinitd.sh: fix syntax error

--- checkinitd.sh
+++ checkinitd.sh
@@ -4,6 +4,6 @@ if [ ! -f /system/bin/sysinit ] || [[ "$(cat /system/bin/sysinit)" != *"init.d"*
     if [ -f /system/xbin/busybox ]; then
         echo -en "\n# init.d\n/system/xbin/busybox run-parts /system/etc/init.d\n">>/system/bin/sysinit
     else
-        echo -en "\n# init.d\nALLINITS="'"$(ls /system/etc/init.d/)"'"\nfor "'$initd in $ALLINITS; do'"\n"'/system/bin/sh /system/etc/init.d/$initd'"\ndone\n">>/system/bin/sysinit
+        echo -en "\n# init.d\nALLINITS="'"$(ls /system/etc/init.d/)"'"\nfor "'initd in $ALLINITS; do'"\n"'/system/bin/sh /system/etc/init.d/$initd'"\ndone\n">>/system/bin/sysinit
     fi
 fi
