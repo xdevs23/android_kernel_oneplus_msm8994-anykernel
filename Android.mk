fa8ddc929431dca53969daad815657bdc882a9d1
Simao Gomes Viana <xdevs23@outlook.com>
2016-08-21 15:25:11 +0200

    Android.mk: build KernelAdiutor and bootimage before anykernel zip

--- Android.mk
+++ Android.mk
@@ -11,13 +11,32 @@ LOCAL_KERNEL_VERSION := blueberry-kernel-v1.4-bh3.6
 LOCAL_KERNEL_AUTHOR := xdevs23
 LOCAL_KERNEL_AUTHOR_ENDSTRING :=
 
+LOCAL_KA_MOVE := true
+LOCAL_KA_TOMOVE := ../KernelAdiutor/
+LOCAL_KA_MVDEST := packages/apps/KernelAdiutor
+
+LOCAL_ANYKERNEL_KA_MOVED := $(shell [ ! -e packages/apps/KernelAdiutor ] && mv $(LOCAL_KA_TOMOVE) $(LOCAL_KA_MVDEST) && \
+	echo -e "${CL_MAG}KernelAdiutor inserted. Please initiate the build again.">&2 && echo -n "true")
+
+ifeq ($(LOCAL_ANYKERNEL_KA_MOVED),true)
+$(error Reinitiaion of build necessary. KernelAdiutor inserted on build time.)
+endif
+
 ifneq ($(LOCAL_KERNEL_AUTHOR),)
     LOCAL_KERNEL_AUTHOR_ENDSTRING = -$(LOCAL_KERNEL_AUTHOR)
 endif
 
 ANYKERNEL_SH := $(LOCAL_DIR)/anykernel.zip
 
-$(ANYKERNEL_SH):
+anykernel-buildka: | bootimage KernelAdiutor
+
+$(ANYKERNEL_SH): | bootimage anykernel-buildka
+	@echo "Copying new KernelAdiutor..."
+	cp $(OUT)/system/app/KernelAdiutor/KernelAdiutor.apk $(LOCAL_DIR)/KernelAdiutor/KernelAdiutor.apk
+	@if [ "$(LOCAL_KA_MOVE)" == "true" ]; then \
+	echo "Moving KA back to its old location"; \
+	mv $(LOCAL_KA_MVDEST) $(LOCAL_KA_TOMOVE); \
+	fi
 	@if [ ! -e "$(OUT)/kernel" ]; then echo -e "\033[91mNo kernel image found! Please build the kernel using 'make bootimage' first.\033[0m"; exit 1; fi
 	@echo "Building AnyKernel package..."
 	cp $(OUT)/kernel $(LOCAL_DIR)/Image
@@ -25,4 +44,8 @@ $(ANYKERNEL_SH):
 	mv $(OUT)/anykernel.zip $(OUT)/$(LOCAL_KERNEL_VERSION)$(LOCAL_KERNEL_AUTHOR_ENDSTRING).zip
 	@echo "AnyKernel package is made."
 
-all_modules: $(ANYKERNEL_SH)
+anykernel-alltargets: | bootimage KernelAdiutor $(ANYKERNEL_SH)
+
+all_modules: anykernel-alltargets
+.PHONY: anykernel-alltargets
+$(LOCAL_MODULE): anykernel-alltargets
